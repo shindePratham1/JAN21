e41da8fd1d2870e9a6758021850107881d495cf7
shinde prathamesh <prathameshshinde2799@gmail.com>
2022-01-24 17:38:53 +0530

    Hello Commit

---
+++ src/test/java/study/test/TestMain.java
@@ -0,0 +1,19 @@
+package study.test;
+
+
+	
+	
+	import org.junit.jupiter.api.Test;
+
+import study.Main;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+	public class TestMain {
+
+	 @Test
+	public void testInputIsEven(){
+	 assertTrue(Main.checkIfInputIsAnEvenNumber(22)); 
+	//Assertion
+	 }
+	 }
+
